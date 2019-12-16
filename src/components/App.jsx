080b87e1fce2dbb104ccfd49be7c7897bf43608e
Sara Talefe <sara.talefe@academiadecodigo.org>
2019-12-16 01:40:25 +0100

    eslint configurations and add navbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './home';
+import Players from './players';
+import Teams from './teams';
+import Navbar from './navbar';
 
+// eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
-  render() {  
+  render() {
     return (
-      <div>
-        React Router Course
-      </div>
+      <Router>
+        <div>
+          <Navbar />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/players" component={Players} />
+            <Route path="/teams" component={Teams} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
